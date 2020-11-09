0e3d1823d7bb188f5cd79aab96194e40fd729f35
Blesswin Samuel <blesswinsamuel@gmail.com>
2020-11-09 15:53:45 +0530

    Add hash info env variables for docker configs

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,9 +1,16 @@
 package config
 
 import (
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+	"io"
 	"io/ioutil"
+	"os"
 	"path"
+	"strings"
 
+	log "github.com/sirupsen/logrus"
 	"gopkg.in/yaml.v2"
 )
 
@@ -19,6 +26,12 @@ type Stack struct {
 	Environment  map[string]string `yaml:"environment"`
 }
 
+type composeFile struct {
+	Configs map[string]struct {
+		File string `yaml:"file"`
+	} `yaml:"configs"`
+}
+
 func ParseConfig(repoDir, stackFile string) (*StackConfig, error) {
 	var stackConfig StackConfig
 	data, err := ioutil.ReadFile(path.Join(repoDir, stackFile))
@@ -29,11 +42,33 @@ func ParseConfig(repoDir, stackFile string) (*StackConfig, error) {
 	if err != nil {
 		return nil, err
 	}
+	h := sha256.New()
 	for _, stack := range stackConfig.Stacks {
 		for i, f := range stack.ComposeFiles {
+			data, err := ioutil.ReadFile(path.Join(repoDir, f))
+			if err != nil {
+				return nil, err
+			}
+			var composeFile composeFile
+			err = yaml.Unmarshal([]byte(data), &composeFile)
+			if err != nil {
+				log.Warn("Ignoring error: %v", err)
+			}
+			for key, c := range composeFile.Configs {
+				configFile := strings.ReplaceAll(c.File, "${PWD}", repoDir)
+				f, err := os.Open(configFile)
+				if err != nil {
+					return nil, fmt.Errorf("failed to open config file '%s': %v", configFile, err)
+				}
+				if _, err := io.Copy(h, f); err != nil {
+					return nil, fmt.Errorf("failed to hash config file '%s': %v", configFile, err)
+				}
+				stack.Environment[key+"_hash"] = hex.EncodeToString(h.Sum(nil))[:7]
+			}
 			stack.ComposeFiles[i] = path.Join(repoDir, f)
 		}
 		stack.Environment["PWD"] = repoDir
+		fmt.Println(stack.Environment)
 	}
 	return &stackConfig, nil
 }
